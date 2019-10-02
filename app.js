a54eba38f8d434a6ba22a506a825287998ba9c7b
kohli <rprasanthreddy120@gmail.com>
2019-10-02 16:11:45 +0530

    hello

---
+++ app.js
@@ -0,0 +1,17 @@
+// var str ="king  ";
+// var str1='kong ';
+// var str3=`goal 
+// <h1>good follow</h>
+// <p> lorem epsum</p>
+// <h1>head</h1>`;
+
+// console.log (str3);
+// document.write(str3);
+
+var s="  anu my love";
+var age="20";
+var sal=10000;
+var com="wipro";
+
+console.log("my name is "+s+"  my age is   "+age+"my salary is   "+sal+"my company is  "+com);
+document.write(`my anme is  ${s} and  my age is ${age} and i work for${com} and iam getting salaary${sal}`);
