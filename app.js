066aacc86b04c127f9b02b0076f4983632d16daa
kohli <rprasanthreddy120@gmail.com>
2019-10-02 18:26:12 +0530

    prasanth

--- app.js
+++ app.js
@@ -8,10 +8,27 @@
 // console.log (str3);
 // document.write(str3);
 
-var s="  anu my love";
-var age="20";
-var sal=10000;
-var com="wipro";
+// var s="  anu my love";
+// var age="20";
+// var sal=10000;
+// var com="wipro";
 
-console.log("my name is "+s+"  my age is   "+age+"my salary is   "+sal+"my company is  "+com);
-document.write(`my anme is  ${s} and  my age is ${age} and i work for${com} and iam getting salaary${sal}`);
+// console.log("my name is "+s+"  my age is   "+age+"my salary is   "+sal+"my company is  "+com);
+// document.write(`my anme is  ${s} and  my age is ${age} and i work for${com} and iam getting salaary${sal}`);
+// console.log(typeof s);
+// console.log(typeof sal);
+// //number
+// var num =10;
+// var num1=10.10;
+// var num2=1/1;
+// console.log(typeof num);
+// console.log(typeof num1);
+// console.log(typeof num2);
+
+
+// var test=true;
+// var test1=false;
+username ="raffi";
+var username;
+console.log(username);
+
