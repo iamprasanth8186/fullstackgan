d998082dfcc86a751b7bbfee2bd114976b4df8cb
kohli <rprasanthreddy120@gmail.com>
2019-10-03 16:51:08 +0530

    non-primitive

--- app.js
+++ app.js
@@ -1,34 +1,91 @@
-// var str ="king  ";
-// var str1='kong ';
-// var str3=`goal 
-// <h1>good follow</h>
-// <p> lorem epsum</p>
-// <h1>head</h1>`;
+// // var str ="king  ";
+// // var str1='kong ';
+// // var str3=`goal 
+// // <h1>good follow</h>
+// // <p> lorem epsum</p>
+// // <h1>head</h1>`;
 
-// console.log (str3);
-// document.write(str3);
+// // console.log (str3);
+// // document.write(str3);
 
-// var s="  anu my love";
-// var age="20";
-// var sal=10000;
-// var com="wipro";
+// // var s="  anu my love";
+// // var age="20";
+// // var sal=10000;
+// // var com="wipro";
 
-// console.log("my name is "+s+"  my age is   "+age+"my salary is   "+sal+"my company is  "+com);
-// document.write(`my anme is  ${s} and  my age is ${age} and i work for${com} and iam getting salaary${sal}`);
-// console.log(typeof s);
-// console.log(typeof sal);
-// //number
-// var num =10;
-// var num1=10.10;
-// var num2=1/1;
-// console.log(typeof num);
-// console.log(typeof num1);
-// console.log(typeof num2);
+// // console.log("my name is "+s+"  my age is   "+age+"my salary is   "+sal+"my company is  "+com);
+// // document.write(`my anme is  ${s} and  my age is ${age} and i work for${com} and iam getting salaary${sal}`);
+// // console.log(typeof s);
+// // console.log(typeof sal);
+// // //number
+// // var num =10;
+// // var num1=10.10;
+// // var num2=1/1;
+// // console.log(typeof num);
+// // console.log(typeof num1);
+// // console.log(typeof num2);
 
 
-// var test=true;
-// var test1=false;
-username ="raffi";
-var username;
-console.log(username);
+// // var test=true;
+// // var test1=false;
+// username ="raffi";
+// var username;
+// console.log(username);
+// non-primitive data types
+// let lang=['java','python',undefined,null,143,'ruby'];
+// var array=['my','own','project','my','style'];
+// var array1=new Array('atp','good','practice');
+// console.log(lang[2]);
+// console.log(array);
+// console.log(array1);
+
+
+//---------length---//
+
+
+
+// var array3=Array(7);
+// console.log(array3);
+// var array4=Array.of(1,2,3,4,5);
+// console.log(array4);
+// var str="pavan is good boy";
+// console.log(typeof(array3));
+// console.log(Array.isArray(str));
+// console.log(Array.isArray(array37))
+
+
+//----string---to Array--3 methods----//
+
+// var str="iam good boy";
+// var strtoarray=str.split(' ');
+// console.log(strtoarray);
+// var es6=Array.from(str);
+// console.log(es6);
+// let[...king]=str;
+// console.log(king);
+// console.log(king.join(''));
+
+//-///---concate array----//////
+
+
+
+// let array1=['array','love'];
+// let array2=['danger','dove'];
+// let concating=array1.concat(array2,'king');
+// console.log(concating);
+
+// let total=[...array1,...array2,'pavan'];
+
+
+//////------reverse the string-------///////
+
+// var str ='good';
+// var reverse=[...str].reverse().join('');
+// console.log(reverse);
+
+
+
+
+
+
 
